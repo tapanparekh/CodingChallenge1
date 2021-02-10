7f93faa60be2a444deea3d23ea0d3e49ac5f7e85
tapan.parekh <sdtapanparekh2016@gmail.com>
2021-02-11 03:09:58 +0530

    Created product page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import bootstrap from "bootstrap";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
